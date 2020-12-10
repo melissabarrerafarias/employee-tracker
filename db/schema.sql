d254af6a97606e6ad16c707881b07d4232b6ae85
melissikiss <melissikiss@gmail.com>
2020-12-10 13:03:36 -0800

    added values for department and role

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,24 @@ CREATE DATABASE business_db;
 USE business_db;
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INTEGER
+    department_id INTEGER,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY, 
+    id INTEGER NOT NULL AUTO_INCREMENT, 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    manager_id INTEGER, 
-);
+    manager_id INTEGER,
+    PRIMARY KEY(id)
+); 
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name)
+VALUES 
+("General Management"),
+("Marketing"),
+("Operations"),
+("Finance"),
+("Sales"),
+("Human Resource"),
+("Purchase");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+("Salesman", 25.3, 5), 
+("Product Distributor", 30.75, 2),
+("Accountant", 25.4, 4),
+("Quality Assurance", 30.75, 7),
+("Training", 25.6, 6),
+("Booking", 30.65, 3),
+("Manager", 20.1, 1);
+
+
+
