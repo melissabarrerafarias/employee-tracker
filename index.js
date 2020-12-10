84b1550700853a04232b84f924c5a9050cce954d
melissikiss <melissikiss@gmail.com>
2020-12-09 19:51:46 -0800

    added inquirer questions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const inquirer = require('inquirer');
+
+function promptUser() {
+    inquirer.prompt(
+        {
+            type: "list", 
+            name: "choices",
+            message: "Hello! Please select one of the following options: ", 
+            choices: [
+                "View all Departments", 
+                "View all Roles", 
+                "View all Employees", 
+                "Add a Department", 
+                "Add a Role", 
+                "Add an Employee",
+                "Update an existing employee role",
+            ],
+        }
+    ).then((answers) => {
+        console.log(answers);
+    });
+}
+
+promptUser();
