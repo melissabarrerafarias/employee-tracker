e5c41aeef18a0543de9f7682a4fbd12f0f79a443
melissikiss <melissikiss@gmail.com>
2020-12-11 09:54:39 -0800

    added queries for seeing all depts, roles, and employees. created loop for returning to menu and exiting app

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
+    // Your MySQL username
     user: 'root',
+    // Your MySQL password
+    password: '',
     database: 'business_db'
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+   console.log("Connected to the database!");
+   promptUser();
   });
 
 function promptUser() {
@@ -22,14 +36,87 @@ function promptUser() {
                 "Add a Role", 
                 "Add an Employee",
                 "Update an existing employee role",
+                "Exit",
             ],
         }
-    ).then((answers) => {
+    ).then(function (answers) {
         console.log(answers);
+        executeRequest(answers.choices);
     });
 }
 
-promptUser();
+function executeRequest(res) {
+    if (res === "View all Departments") {
+        viewDepartments();
+    }
+
+    else if (res === "View all Roles") {
+        viewRoles();
+    }
+
+    else if (res === "View all Employees") {
+        viewEmployees();
+    }
+}
+
+function viewDepartments() {
+    console.log("Viewing all Departments...");
+    connection.query("SELECT * FROM department",
+    function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        returnMenu();
+      });
+};
+
+function viewRoles() {
+    console.log("Viewing all Roles...");
+    connection.query("SELECT * FROM role",
+    function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        returnMenu();
+      });
+};
+
+function viewEmployees() {
+    console.log("Viewing all Employees");
+    connection.query("SELECT * FROM employee",
+    function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        returnMenu();
+    });
+}
+
+// function addDepartment() {
+    
+// }
+
+function returnMenu() {
+    inquirer.prompt(
+        {
+            type: "confirm", 
+            name: "confirmMenu", 
+            message: "Would you like to return to the menu?",
+            default: true
+        },
+    ) .then(function (answers) {
+        if (answers.confirmMenu) {
+            promptUser();
+        }
+        else {
+            exitApp();
+        }
+    });
+}
+
+
+function exitApp() {
+    console.log("Goodbye");
+}
+
+
 
 
 // to start the mysql server run 'mysql -u root -p' and then do source db/schema.sql and to check do 'show databases;' and 'show tables;'
